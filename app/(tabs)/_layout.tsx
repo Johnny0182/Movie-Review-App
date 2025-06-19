b0cff169c1dcee98a2aa80a9cb7b3a24d71247ce
johnny0182 <johnny.leyva182@gmail.com>
2025-06-18 19:57:40 -0700

    Search Bar : )

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,20 +7,21 @@ import { Image, ImageBackground, Text, View } from 'react-native';
 const TabIcon = ({ focused, icon, title}: any) => {
     if (focused) {
         return (
-            <ImageBackground
-                source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center
-                items-center rounded-full overflow-hidden"
-            >
-                <Image source={icon} tintColor="#151312" className="size-5" />
-                <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
-            </ImageBackground>
+            <View className="flex-1 justify-center items-center mt-4">
+                <ImageBackground
+                    source={images.highlight}
+                    className="flex flex-row justify-center items-center rounded-full overflow-hidden px-4"
+                    style={{ width: 110, height: 40 }} // Fixed width AND height
+                >
+                    <Image source={icon} tintColor="#151312" className="size-5" />
+                    <Text className="text-secondary text-base font-semibold ml-2" numberOfLines={1}>{title}</Text>
+                </ImageBackground>
+            </View>
         )
     }
-
+    
     return (
-        <View className="size-full justify-center items-center
-        mt-4 rounded-full">
+        <View className="size-full justify-center items-center mt-4 rounded-full">
             <Image source={icon} tintColor="#A8B5DB" className="size-5" />
         </View>
     )
