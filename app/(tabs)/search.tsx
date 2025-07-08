b1b1c53d2488743ad37ac2a134e5841c14cc9257
johnny0182 <johnny.leyva182@gmail.com>
2025-07-07 23:06:55 -0700

    added database (appwrite)

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
 import { images } from '@/constants/images';
 import { fetchMovies } from '@/services/api';
+import { updateSearchCount } from '@/services/appwrite';
 import useFetch from '@/services/useFetch';
 import { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
@@ -16,14 +17,22 @@ const Search = () => {
     error,
     refetch: loadMovies,
     reset,
-  } = useFetch(() => fetchMovies({
-    query: searchQuery
-  }), false);
+  } = useFetch(
+    () =>
+      fetchMovies({
+        query: searchQuery,
+      }),
+    false
+  );
 
   useEffect(() => {
+    updateSearchCount(searchQuery, movies);
+
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+        if (movies?.length > 0 && movies?.[0])
+          await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset();
       }
@@ -40,8 +49,12 @@ const Search = () => {
 
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
-      
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+
       <FlatList
         data={movies}
         renderItem={({ item }) => <MovieCard {...item} />}
@@ -51,7 +64,7 @@ const Search = () => {
         columnWrapperStyle={{
           justifyContent: 'center',
           gap: 16,
-          marginVertical: 16
+          marginVertical: 16,
         }}
         contentContainerStyle={{ paddingBottom: 100 }}
         ListHeaderComponent={
@@ -59,17 +72,21 @@ const Search = () => {
             <View className="w-full flex-row justify-center mt-20 items-center">
               <Image source={icons.logo} className="w-12 h-10 mb-5" />
             </View>
-              
+
             <View className="mb-5">
-              <SearchBar 
-                placeholder="Search movies ..." 
+              <SearchBar
+                placeholder="Search movies ..."
                 value={searchQuery}
                 onChangeText={(text: string) => setSearchQuery(text)}
               />
             </View>
 
             {loading && (
-              <ActivityIndicator size="large" color="#0000ff" className="my-3" />
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
             )}
 
             {error && (
@@ -80,27 +97,33 @@ const Search = () => {
 
             {searchQuery.trim() && !loading && (
               <Text className="text-xl text-white font-bold px-5 my-3">
-                Search Results for {' '}
+                Search Results for{' '}
                 <Text className="text-accent">{searchQuery}</Text>
               </Text>
             )}
 
-            {!loading && !error && searchQuery.trim() && movies?.length === 0 && (
-              <Text className="text-sm text-gray-400 px-5">
-                No movies found for "{searchQuery}"
-              </Text>
-            )}
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length === 0 && (
+                <Text className="text-sm text-gray-400 px-5">
+                  No movies found for "{searchQuery}"
+                </Text>
+              )}
 
-            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
-              <Text className="text-sm text-green-500 px-5">
-                Found {movies.length} movies
-              </Text>
-            )}
+            {!loading &&
+              !error &&
+              searchQuery.trim() &&
+              movies?.length > 0 && (
+                <Text className="text-sm text-green-500 px-5">
+                  Found {movies.length} movies
+                </Text>
+              )}
           </>
         }
       />
     </View>
   );
-}
+};
 
 export default Search;
