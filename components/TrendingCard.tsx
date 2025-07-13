e6e3f47f8d4742d9e7d8972d5d084dbfb7157c65
johnny0182 <johnny.leyva182@gmail.com>
2025-07-13 04:10:19 -0700

    Trending Movie Cards finished. (Numbered cards up to 10)

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,12 +1,18 @@
+import { images } from "@/constants/images";
+import MaskedView from '@react-native-masked-view/masked-view';
 import { Link } from "expo-router";
 import React from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 const TrendingCard = ({ movie: { movie_id, title, poster_url }, index }: TrendingCardProps) => {
   // Only render if poster_url exists and is valid
   if (!poster_url || poster_url.includes('${movie.poster_path}')) {
     return null;
   }
+
+  // Adjust position for double digits
+  const isDoubleDigit = index + 1 >= 10;
+  const numberPosition = isDoubleDigit ? "-left-4" : "-left-3.5";
 
   return (
     <Link href={`/movies/${movie_id}`} asChild>
@@ -16,6 +22,20 @@ const TrendingCard = ({ movie: { movie_id, title, poster_url }, index }: Trendin
           className="w-32 h-48 rounded-lg"
           resizeMode="cover"
         />
+        <View className={`absolute bottom-9 ${numberPosition} px-2 py-3 rounded-full`}>
+            <MaskedView maskElement={
+              <Text className={`font-bold text-white ${isDoubleDigit ? 'text-5xl' : 'text-6xl'}`}>
+                {index + 1}
+              </Text>
+            }>
+              <Image source={images.rankingGradient} 
+              className="size-14" 
+              resizeMode="cover" />
+            </MaskedView>
+        </View>
+        <Text className="text-sm font-bold mt-2 text-light-200 capitalize" numberOfLines={2}>
+          {title}
+        </Text>
       </TouchableOpacity>
     </Link>
   );
