ac72582a12faf8e853e5cfb30dd794e2538178f8
johnny0182 <johnny.leyva182@gmail.com>
2025-07-14 19:41:02 -0700

    search bar fix

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { icons } from '@/constants/icons';
 import React from 'react';
-import { Image, TextInput, View } from 'react-native';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 interface Props {
   placeholder: string;
@@ -10,19 +10,36 @@ interface Props {
 }
 
 const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
+  // If onPress is provided (like on home page), make it a touchable that navigates
+  if (onPress && !onChangeText) {
+    return (
+      <TouchableOpacity 
+        onPress={onPress}
+        className="flex-row items-center bg-dark-200 rounded-full px-5 py-4"
+      >
+        <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+        <View className="flex-1 ml-2">
+          <Text className="text-[#a8b5db]">{placeholder}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  // If onChangeText is provided (like on search page), make it an actual input
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-        <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
-    
-        <TextInput
-          onPress={onPress}
-          placeholder={placeholder}
-          value={value}
-          onChangeText={onChangeText}
-          placeholderTextColor="#a8b5db"
-          className="flex-1 ml-2 text-white"
-        />
+      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
+      
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+        autoFocus={true}
+      />
     </View>
-  )
+  );
 }
+
 export default SearchBar
